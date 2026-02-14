afd096df1219dfc615505a5e499dacbcc29d54eb
banne <baidyanne9@gmail.com>
2026-02-14 17:30:11 +0000

    creation dossiers weapons et raycasting + implementation des armes

---
+++ src/weapons/draw_weapons.c
@@ -0,0 +1,20 @@
+#include "../../includes/cub3d.h"
+
+void	draw_weapon(t_game *game)
+{
+    int x = (game->win_w - 64) / 2;
+    int y = game->win_h - 128;
+
+    if (game->weapon.type == 0)
+        mlx_put_image_to_window(game->mlx, game->win, game->weapon.knife.img[0], x, y);
+    else if (game->weapon.type == 1)
+        mlx_put_image_to_window(game->mlx, game->win, game->weapon.gun.img, x, y);
+}
+
+void swap_weapon(t_game *game)
+{
+    if (game->weapon.type == 0)
+        game->weapon.type = 1;
+    else
+        game->weapon.type = 0;
+}
