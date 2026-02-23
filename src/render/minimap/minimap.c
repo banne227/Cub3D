0ca0429dc54e0993d8878bd7f9403775376165ad
Baidy Anne <banne@c1r3p12.42lehavre.fr>
2026-02-23 14:14:46 +0100

    norm

--- src/render/minimap/minimap.c
+++ src/render/minimap/minimap.c
@@ -6,18 +6,21 @@
 /*   By: banne <banne@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 10:50:21 by banne             #+#    #+#             */
-/*   Updated: 2026/02/23 13:40:54 by banne            ###   ########.fr       */
+/*   Updated: 2026/02/23 14:14:39 by banne            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static void	put_pixel_to_img(t_game *game, int *img_data, int line_len,
-	t_pos pos, int color)
+static void	put_pixel_to_img(t_game *game, t_pos pos, int color)
 {
 	int	x;
 	int	y;
+	int	*img_data;
+	int	line_len;
 
+	img_data = (int *)game->addr;
+	line_len = game->line_length;
 	x = pos.x;
 	y = pos.y;
 	if (x < 0 || x >= game->win_w || y < 0 || y >= game->win_h)
@@ -25,8 +28,7 @@ static void	put_pixel_to_img(t_game *game, int *img_data, int line_len,
 	img_data[y * (line_len / 4) + x] = color;
 }
 
-void	draw_player_direction(t_game *game, int *img_data, int line_len,
-		t_pos player_pos)
+void	draw_player_direction(t_game *game, t_pos player_pos)
 {
 	int		i;
 	t_pos	pos;
@@ -36,12 +38,12 @@ void	draw_player_direction(t_game *game, int *img_data, int line_len,
 	{
 		pos.x = player_pos.x + (int)(game->player.dir_x * i);
 		pos.y = player_pos.y + (int)(game->player.dir_y * i);
-		put_pixel_to_img(game, img_data, line_len, pos, 0x00FF00);
+		put_pixel_to_img(game, pos, 0x00FF00);
 		i++;
 	}
 }
 
-void	print_player_pos(t_game *game, int *img_data, int line_len)
+void	print_player_pos(t_game *game)
 {
 	int	color;
 	int	i;
@@ -56,14 +58,14 @@ void	print_player_pos(t_game *game, int *img_data, int line_len)
 		j = -size;
 		while (j <= size)
 		{
-			put_pixel_to_img(game, img_data, line_len,
+			put_pixel_to_img(game,
 				(t_pos){(int)(game->player.pos_x * MINI_TILE) + i,
 				(int)(game->player.pos_y * MINI_TILE) + j}, color);
 			j++;
 		}
 		i++;
 	}
-	draw_player_direction(game, img_data, line_len,
+	draw_player_direction(game,
 		(t_pos){(int)(game->player.pos_x * MINI_TILE),
 		(int)(game->player.pos_y * MINI_TILE)});
 }
@@ -72,12 +74,7 @@ void	draw_minimap(t_game *game)
 {
 	t_mini	mini;
 	int		color;
-	int		*img_data;
 
-	img_data = (int *)mlx_get_data_addr(game->img, &mini.bpp,
-			&mini.line_len, &mini.endian);
-	if (!img_data)
-		return ;
 	mini.y = 0;
 	while (mini.y < game->map.height * MINI_TILE)
 	{
@@ -89,10 +86,9 @@ void	draw_minimap(t_game *game)
 				color = WALL_COLOR;
 			else
 				color = FLOOR_COLOR;
-			put_pixel_to_img(game, img_data, mini.line_len,
-				(t_pos){mini.x++, mini.y}, color);
+			put_pixel_to_img(game, (t_pos){mini.x++, mini.y}, color);
 		}
 		mini.y++;
 	}
-	print_player_pos(game, img_data, mini.line_len);
+	print_player_pos(game);
 }
